0dfd5eb82a8116327950cad694f6775a69567c55
William Emfinger <emfinger@isis.vanderbilt.edu>
2016-04-12 14:45:06 -0700

    updated to fix negative execution times and multiple copies of same operation in the queue.  fixes #57

--- src/visualizers/widgets/ResultsViz/ResultsVizWidget.js
+++ src/visualizers/widgets/ResultsViz/ResultsVizWidget.js
@@ -69,7 +69,7 @@ define([
 		var p = document.createElement('div');
 		p.id = "log_plot_" + a;
 		p.style = 'width:1300px;height:300px';
-		var re = /ROSMOD::(\w+)::([\d]*)::([\w]+ ?)*::Alias=(\w+); (?:(?:[\w=;, ]*Enqueue Time)|(?:Completion Time)) sec=(\d*), nsec=(\d*)/gi;
+		var re = /ROSMOD::(\w+)::([\d]*)::((?:CALLBACK COMPLETED)|(?:CALLBACK FIFO ENQUEUE) ?)*::Alias=(\w+); (?:(?:[\w=;, ]*Enqueue Time)|(?:Completion Time)) sec=(\d*), nsec=(\d*)/gi;
 		var result = re.exec(desc.attributes[a]);
 		var log_data = {};
 		var first_time = 0.0;
@@ -80,34 +80,35 @@ define([
 		    var alias = result[4];
 		    if (!log_data[alias]) {
 			log_data[alias] = {
-			    data : []
+			    data : [],
+			    enqueue_times: [],
+			    completion_times: []
 			};
 		    }
-		    if (result[3] == "ENQUEUE") {
+		    if (result[3].indexOf('ENQUEUE') > -1) {
 			var enqueue_time = parseInt(result[5]) + result[6]/1000000000.0;
-			log_data[alias].enqueue_time = enqueue_time;
+			log_data[alias].enqueue_times.push(enqueue_time);
 			if (first_time == 0.0)
 			    first_time = enqueue_time;
 		    }
-		    else if (result[3] == "COMPLETED") {
+		    else if (result[3].indexOf('COMPLETED') > -1) {
 			var completion_time = parseInt(result[5]) + result[6]/1000000000.0;
-			log_data[alias].completion_time = completion_time;
+			log_data[alias].completion_times.push(completion_time);
 		    }
-		    if ( log_data[alias].completion_time && log_data[alias].enqueue_time ) {
-			var exec_time = log_data[alias].completion_time - 
-			    log_data[alias].enqueue_time;
+		    if ( log_data[alias].completion_times.length > 0 && log_data[alias].enqueue_times.length > 0 ) {
+			// at this point there can only be one completion_time, though there may be multiple enqueue times
+			var comp = log_data[alias].completion_times[0];
+			var enq = log_data[alias].enqueue_times[0];
+			var exec_time = comp - enq;
 			if (exec_time > max_exec_time)
 			    max_exec_time = exec_time;
-			log_data[alias].data.push([log_data[alias].enqueue_time - first_time, 
-						   0]);
-			log_data[alias].data.push([log_data[alias].enqueue_time - first_time, 
-						   exec_time]);
-			log_data[alias].data.push([log_data[alias].completion_time - first_time, 
-						   exec_time]);
-			log_data[alias].data.push([log_data[alias].completion_time - first_time, 
-						   0]);
-			log_data[alias].enqueue_time = undefined;
-			log_data[alias].completion_time = undefined;
+			log_data[alias].data.push([enq - first_time,  0]);
+			log_data[alias].data.push([enq - first_time,  exec_time]);
+			log_data[alias].data.push([comp - first_time, exec_time]);
+			log_data[alias].data.push([comp - first_time, 0]);
+			// remove the currently processed enqueue / completion time
+			log_data[alias].enqueue_times = log_data[alias].enqueue_times.slice(1);
+			log_data[alias].completion_times = log_data[alias].completion_times.slice(1);
 		    }
 		    result = re.exec(desc.attributes[a]);
 		}
