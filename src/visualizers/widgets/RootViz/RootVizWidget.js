0e8e346bc13b8fb619bf5bf6510056ff017d0480
William Emfinger <emfinger@isis.vanderbilt.edu>
2016-04-14 14:46:42 -0500

    updated to allow creation of projects by dragging and dropping into the root. closes #53

--- src/visualizers/widgets/RootViz/RootVizWidget.js
+++ src/visualizers/widgets/RootViz/RootVizWidget.js
@@ -7,54 +7,46 @@
 
 define([
     'text!./RootViz.html',
-    'js/DragDrop/DragHelper',
-    //'js/Widgets/ModelEditor/ModelEditorWidget',
+    'js/DragDrop/DropTarget',
+    'js/DragDrop/DragConstants',
     'common/util/ejs',
-    './Buttons',
     './Templates',
+    'js/Utils/GMEConcepts',
+    'js/NodePropertyNames',
     'css!./styles/RootVizWidget.css'
 ], function (
     RootVizHtml,
-    DragHelper,
-    //ModelEditorWidget,
+    dropTarget,
+    DROP_CONSTANTS,
     ejs,
-    Buttons,
-    TEMPLATES) {
+    TEMPLATES,
+    GMEConcepts,
+    nodePropertyNames) {
     'use strict';
 
     var RootVizWidget,
         WIDGET_CLASS = 'root-viz';
 
-    RootVizWidget = function (logger, container, params) {
+    RootVizWidget = function (logger, container, client) {
         this._logger = logger.fork('Widget');
 
-	/*
-	params = params || {};
-	params.tabsEnabled = false;
-	params.addTabs = false;
-	params.deleteTabs = false;
-	params.reorderTabs = false;
-	params.droppable = true;
-	params.zoomvalues = [0.1,0.5,1.0,1.5,2.0];
-
-	ModelEditorWidget.call(this, container, params);
-	*/
-
         this.$el = container;
+
+	this._client = client;
 
         this.nodes = {};
 
         this._initialize();
+	this._makeDroppable();
 
         this._logger.debug('ctor finished');
     };
-
-    //_.extend(RootVizWidget.prototype, ModelEditorWidget.prototype);
 
     RootVizWidget.prototype._initialize = function () {
         // set widget class
         this.$el.addClass(WIDGET_CLASS);
         this.$el.append(RootVizHtml);
+	this.$table = this.$el.find('#rootVizTable');
 
 	this._nodes = [];
 	this._numNodes = 0;
@@ -62,33 +54,21 @@ define([
 	this._tableSetup = false;
     };
 
-    RootVizWidget.prototype.onWidgetContainerResize = function (width, height) {
-	/*
-	this._logger.error('RESIZING:: ' + width + ' ' + height);
-	this._initialize(width);
-	this._nodes.map(function(desc) {
-	    this.createNodeEntry(desc);
-	});
-	*/
-    };
-
     RootVizWidget.prototype.setupTable = function() {
 	var sizeOfElement = 300;
         var width = this.$el.width(),
             height = this.$el.height();
 	this._numElementsPerRow = Math.floor(width / sizeOfElement);
-	var table = this.$el.find('#rootVizTable');
-	table.empty();
-	table.append('<colgroup>');
+	this.$table.empty();
+	this.$table.append('<colgroup>');
 	for (var i=0;i<this._numElementsPerRow;i++)
-	    table.append('<col width="'+100/this._numElementsPerRow+'%" height="auto">');
-	table.append('</colgroup>');
+	    this.$table.append('<col width="'+100/this._numElementsPerRow+'%" height="auto">');
+	this.$table.append('</colgroup>');
 	this._tableSetup = true;
     };
 
     RootVizWidget.prototype.createNodeEntry = function (desc) {
-	var table,
-	row,
+	var row,
 	column,
 	projectHtml,
 	panelId,
@@ -104,8 +84,7 @@ define([
 
 	if ((this._numNodes % this._numElementsPerRow) == 0) {
 	    this._currentRow++;
-	    table = this.$el.find('#rootVizTable');
-	    table.append('<tr id="rowClass'+this._currentRow+'"></tr>');
+	    this.$table.append('<tr id="rowClass'+this._currentRow+'"></tr>');
 	}
 	row = this.$el.find('#rowClass' + this._currentRow);
 	row.append('<td style="vertical-align: top" id="colClass'+this._numNodes+'"></td>');
@@ -146,6 +125,16 @@ define([
 	this._numNodes++;
     };
 
+    RootVizWidget.prototype.onWidgetContainerResize = function (width, height) {
+       /*
+       this._logger.error('RESIZING:: ' + width + ' ' + height);
+       this._initialize(width);
+       this._nodes.map(function(desc) {
+           this.createNodeEntry(desc);
+       });
+       */
+    };
+
     // Adding/Removing/Updating items
     var NODE_WHITELIST = {
         Project: true
@@ -171,7 +160,106 @@ define([
     RootVizWidget.prototype.updateNode = function (desc) {
     };
 
+    RootVizWidget.prototype._isValidDrop = function (dragInfo) {
+	var self = this;
+        var result = false,
+        draggedNodePath,
+	nodeObj,
+	nodeName,
+	metaObj,
+	metaName;
+
+        if (dragInfo[DROP_CONSTANTS.DRAG_ITEMS].length === 1) {
+            draggedNodePath = dragInfo[DROP_CONSTANTS.DRAG_ITEMS][0];
+	    nodeObj = self._client.getNode(draggedNodePath);
+	    nodeName = nodeObj.getAttribute('name');
+	    metaObj = self._client.getNode(nodeObj.getMetaTypeId());
+	    if (metaObj) {
+		metaName = metaObj.getAttribute('name');
+	    }
+            result = metaName && metaName == 'Project';
+        }
+
+        return result;
+    };
+
+
+    // This next function retrieves the relevant node information for the widget
+    RootVizWidget.prototype._getObjectDescriptor = function (nodeId) {
+        var nodeObj = this._client.getNode(nodeId),
+            objDescriptor;
+
+        if (nodeObj) {
+	    var metaObj = this._client.getNode(nodeObj.getMetaTypeId()),
+	    metaName = undefined;
+	    if (metaObj) {
+		metaName = metaObj.getAttribute(nodePropertyNames.Attributes.name);
+	    }
+
+            objDescriptor = {
+                'id': undefined,
+                'name': undefined,
+		'meta': undefined,
+                'childrenIds': undefined,
+                'parentId': undefined,
+                'isConnection': false
+            };
+
+            objDescriptor.id = nodeObj.getId();
+            objDescriptor.name = nodeObj.getAttribute(nodePropertyNames.Attributes.name);
+	    objDescriptor.brief = nodeObj.getAttribute('Brief Description');
+	    objDescriptor.detailed = nodeObj.getAttribute('Detailed Description');
+	    objDescriptor.authors = nodeObj.getAttribute('Authors');
+	    objDescriptor.meta = metaName;
+            objDescriptor.childrenIds = nodeObj.getChildrenIds();
+            objDescriptor.childrenNum = objDescriptor.childrenIds.length;
+            objDescriptor.parentId = nodeObj.getParentId();
+            objDescriptor.isConnection = GMEConcepts.isConnection(nodeId);  // GMEConcepts can be helpful
+        }
+
+        return objDescriptor;
+    };
+
+    RootVizWidget.prototype.createProject = function(basePath) {
+	var client = this._client;
+	var nodeId = '', // root node always exists and always has path ''
+	baseId = client.getNode(basePath).getId();
+	var childCreationParams = {
+	    parentId: nodeId,  // Should be ROOT
+	    baseId: baseId,    // should be META:Project
+	};
+	var childId = client.createChild(childCreationParams, 'Creating new Project');
+	return childId;
+    };
+
     /* * * * * * * * Visualizer event handlers * * * * * * * */
+
+    RootVizWidget.prototype._makeDroppable = function () {
+	var self = this,
+	newProjectId,
+	desc;
+        self.$el.addClass('drop-area');
+        //self._div.append(self.__iconAssignNullPointer);
+
+        dropTarget.makeDroppable(self.$el, {
+            over: function (event, dragInfo) {
+                if (self._isValidDrop(dragInfo)) {
+                    self.$el.addClass('accept-drop');
+                } else {
+                    self.$el.addClass('reject-drop');
+                }
+            },
+            out: function (/*event, dragInfo*/) {
+                self.$el.removeClass('accept-drop reject-drop');
+            },
+            drop: function (event, dragInfo) {
+                if (self._isValidDrop(dragInfo)) {
+		    newProjectId = self.createProject(dragInfo[DROP_CONSTANTS.DRAG_ITEMS][0]);
+                }
+                self.$el.removeClass('accept-drop reject-drop');
+            }
+        });
+    };
 
     RootVizWidget.prototype.onNodeClick = function (id) {
         // This currently changes the active node to the given id and
