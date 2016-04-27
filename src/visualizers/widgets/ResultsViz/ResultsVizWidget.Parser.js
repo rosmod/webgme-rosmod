d6c34500f11a2003635a1c9cd02ab9cfa41f67b0
William Emfinger <emfinger@isis.vanderbilt.edu>
2016-04-27 10:53:13 -0700

    updated parser to handle new format for trace logs.

--- src/visualizers/widgets/ResultsViz/ResultsVizWidget.Parser.js
+++ src/visualizers/widgets/ResultsViz/ResultsVizWidget.Parser.js
@@ -4,47 +4,26 @@ define([], function() {
     'use strict';
     return {
 	getDataFromAttribute: function(attribute) {
-	    var re = /ROSMOD::(\w+)::([\d]*)::((?:CALLBACK COMPLETED)|(?:CALLBACK FIFO ENQUEUE) ?)*::Alias=(\w+); (?:(?:[\w=;, ]*Enqueue Time)|(?:Completion Time)) sec=(\d*), nsec=(\d*)/gi;
+	    var re = /((?:[^\r\n,+\-\(\)\\\/][\S])+),([0-9\.]+),([0-9\.]+),([0-9\.]+),([0-9\.]+),([0-9\.]+)/gi;
 	    var result = re.exec(attribute);
 	    var log_data = {};
 	    var first_time = 0.0;
-	    var max_exec_time = 0.0;
 	    if (result != null)
-		var first_time = parseInt(result[5]) + result[6]/1000000000.0;
+		var first_time = parseFloat(result[2]);
 	    while(result != null) {
-		var alias = result[4];
+		var alias = result[1];
 		if (!log_data[alias]) {
 		    log_data[alias] = {
 			data : [],
-			enqueue_times: [],
-			completion_times: []
 		    };
 		}
-		if (result[3].indexOf('ENQUEUE') > -1) {
-		    var enqueue_time = parseInt(result[5]) + result[6]/1000000000.0;
-		    log_data[alias].enqueue_times.push(enqueue_time);
-		    if (first_time == 0.0)
-			first_time = enqueue_time;
-		}
-		else if (result[3].indexOf('COMPLETED') > -1) {
-		    var completion_time = parseInt(result[5]) + result[6]/1000000000.0;
-		    log_data[alias].completion_times.push(completion_time);
-		}
-		if ( log_data[alias].completion_times.length > 0 && log_data[alias].enqueue_times.length > 0 ) {
-		    // at this point there can only be one completion_time, though there may be multiple enqueue times
-		    var comp = log_data[alias].completion_times[0];
-		    var enq = log_data[alias].enqueue_times[0];
-		    var exec_time = comp - enq;
-		    if (exec_time > max_exec_time)
-			max_exec_time = exec_time;
-		    log_data[alias].data.push([enq - first_time,  0]);
-		    log_data[alias].data.push([enq - first_time,  exec_time]);
-		    log_data[alias].data.push([comp - first_time, exec_time]);
-		    log_data[alias].data.push([comp - first_time, 0]);
-		    // remove the currently processed enqueue / completion time
-		    log_data[alias].enqueue_times = log_data[alias].enqueue_times.slice(1);
-		    log_data[alias].completion_times = log_data[alias].completion_times.slice(1);
-		}
+		var enq = parseFloat(result[2]);
+		var comp = parseFloat(result[4]);
+		var exec_time = parseFloat(result[5]);
+		log_data[alias].data.push([enq - first_time,  0]);
+		log_data[alias].data.push([enq - first_time,  exec_time]);
+		log_data[alias].data.push([comp - first_time, exec_time]);
+		log_data[alias].data.push([comp - first_time, 0]);
 		result = re.exec(attribute);
 	    }
 	    return log_data;
