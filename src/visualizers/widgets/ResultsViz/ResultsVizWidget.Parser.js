43f284ab84c4c62aa05f6ee49b29a229ac3f8e2e
William Emfinger <waemfinger@gmail.com>
2016-08-04 21:44:03 -0500

    fixed parser to be original.

--- src/visualizers/widgets/ResultsViz/ResultsVizWidget.Parser.js
+++ src/visualizers/widgets/ResultsViz/ResultsVizWidget.Parser.js
@@ -1,24 +1,28 @@
 
 
+
 define([], function() {
     'use strict';
     return {
 	getDataFromAttribute: function(attribute) {
-	    var re = /ROSMOD::DEBUG::(\d)+::(\d)+/gi
+	    var re = /((?:[^:\r\n,+\-\(\)\\\/])*), ([0-9\.]+), ([0-9\.]+), ([0-9\.]+), ([0-9\.]+), ([0-9\.]+)/gi;
 	    var result = re.exec(attribute);
 	    var log_data = {};
 	    while(result != null) {
-		var alias = 'log'
+		var alias = result[1];
 		if (!log_data[alias]) {
 		    log_data[alias] = {
 			name : alias,
 			data : [],
 		    };
 		}
-		var time = parseFloat(result[1]);
-		time = time / 1000000000.0; // since it's in nanoseconds
-		var data = parseFloat(result[2]);
-		log_data[alias].data.push([time, data]);
+		var enq = parseFloat(result[2]);
+		var comp = parseFloat(result[4]);
+		var exec_time = parseFloat(result[5]);
+		log_data[alias].data.push([enq,  0]);
+		log_data[alias].data.push([enq,  exec_time]);
+		log_data[alias].data.push([comp, exec_time]);
+		log_data[alias].data.push([comp, 0]);
 		result = re.exec(attribute);
 	    }
 	    return log_data;
