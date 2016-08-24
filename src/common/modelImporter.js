f60088931e3fb954b3c900457c0ed7883963d4c0
William Emfinger <waemfinger@gmail.com>
2016-08-24 11:20:48 -0500

    working on model importer.

--- src/common/modelImporter.js
+++ src/common/modelImporter.js
@@ -5,142 +5,46 @@ define(['q'], function(Q) {
     return {
 	// create all model nodes and their attributes
 	// update all pointers and sets
-	importModel: function(core, model) {
+	importModel: function(core, model, rootNode) {
 	    var self = this;
+	    self.model = model;
+	    self.core = core;
+	    self.rootNode = rootNode;
 
-	    var createdObjects = []; // list of paths?
+	    self.createdObjects = []; // list of paths?
 	    // how do I handle the paths that I'm loading vs. the
-	    // paths that exist in the model already?
+	    // paths that exist in the model already? - don't care?
+	    // just store which I've created, not what they've turned
+	    // into
 
-	    model.modelObjects.map((modelObject) => {
-		// get meta object
-		// make sure parent exists or create it
-		// if object doesn't exist create object
-		// create attributes
-		// add to createdObjects
+	    var modelObjectPaths = self.model.modelObjects.keys;
+	    modelObjectPaths.map((modelObjectPath) => {
+		self.createObject(modelObjectPath);
 	    });
-
-	    var nodeName = core.getAttribute(modelNode, 'name'),
-	    nodePath = core.getPath(modelNode),
-	    nodeType = core.getAttribute(core.getBaseType(modelNode), 'name'),
-	    parentPath = core.getPath(core.getParent(modelNode)),
-	    attributes = core.getAttributeNames(modelNode),
-	    childPaths = core.getChildrenPaths(modelNode),
-	    pointers = core.getPointerNames(modelNode),
-	    sets = core.getSetNames(modelNode);
-
-	    self.model = {
-		name: nodeName,
-		path: nodePath,
-		type: nodeType,
-		parentPath: parentPath,
-		childPaths: childPaths,
-		attributes: {},
-		pointers: {},
-		sets: {},
-		pathDict: {}  // for storing a path->obj dict for all objects in model
-	    };
-	    attributes.map(function(attribute) {
-		var val = core.getAttribute(modelNode, attribute);
-		self.model.attributes[attribute] = val;
-		self.model[attribute] = val;
-	    });
-	    pointers.map(function(pointer) {
-		self.model.pointers[pointer] = core.getPointerPath(modelNode, pointer);
-	    });
-	    sets.map(function(set) {
-		self.model.sets[set] = core.getMemberPaths(modelNode, set);
-	    });
-	    modelObjects.push(self.model);
-	    return core.loadSubTree(modelNode)
-		.then(function(nodes) {
-		    nodes.map(function(node) {
-			nodeName = core.getAttribute(node, 'name');
-			nodePath = core.getPath(node);
-			nodeType = core.getAttribute(core.getBaseType(node), 'name');
-			parentPath = core.getPath(core.getParent(node));
-			attributes = core.getAttributeNames(node);
-			childPaths = core.getChildrenPaths(node);
-			pointers = core.getPointerNames(node);
-			sets = core.getSetNames(node);
-			var nodeObj = {
-			    name: nodeName,
-			    path: nodePath,
-			    type: nodeType,
-			    parentPath: parentPath,
-			    childPaths: childPaths,
-			    attributes: {},
-			    pointers: {},
-			    sets: {}
-			};
-			attributes.map(function(attribute) {
-			    var val = core.getAttribute(node, attribute);
-			    nodeObj.attributes[attribute] = val;
-			    nodeObj[attribute] = val;
-			});
-			pointers.map(function(pointer) {
-			    nodeObj.pointers[pointer] = core.getPointerPath(node, pointer);
-			});
-			sets.map(function(set) {
-			    nodeObj.sets[set] = core.getMemberPaths(node, set);
-			});
-			modelObjects.push(nodeObj);
-			self.model.pathDict[nodeObj.path] = nodeObj;
-		    });
-		    self.resolvePointers(modelObjects);
-		    self.processModel(self.model);
-		    return self.model;
-		});
+	    return true;
 	},
-	resolvePointers: function(modelObjects) {
-	    modelObjects.map(function(obj) {
-		// Can't follow parent path: would lead to circular data structure (not stringifiable)
-		// follow children paths, these will always have been loaded
-		obj.childPaths.map(function(childPath) {
-		    var dst = modelObjects.filter(function (c) { 
-			return c.path == childPath; 
-		    })[0];
-		    if (dst) {
-			var key = dst.type + '_list';
-			if (!obj[key]) {
-			    obj[key] = [];
-			}
-			obj[key].push(dst);
-		    }
-		});
-		// follow pointer paths, these may not always be loaded!
-		for (var pointer in obj.pointers) {
-		    var path = obj.pointers[pointer];
-		    var dst = modelObjects.filter(function (c) { 
-			return c.path == path;
-		    })[0];
-		    if (dst)
-			obj[pointer] = dst;
-		    else if (pointer != 'base' && path != null) 
-			throw new String(obj.name + ' has pointer ' +pointer+ ' to object not in the tree!');
-		    else if (path == null) 
-			throw new String(obj.name + ' has null pointer ' + pointer);
+	createObject: function(objectPath) {
+	    var self = this;
+	    var modelObject = self.model.modelObjects[objectPath];
+	    var metaType = modelObject.type;
+	    var parentNode = self.rootNode;
+	    var newNode = null;
+	    if (self.createdObjects.indexOf(objectPath) == -1) {
+		if (self.createdObjects.indexOf(modelObject.parentPath) == -1) {
+		    // create parent
+		    parentNode = self.createObject(modelObject.parentPath);
 		}
-		// follow set paths, these may not always be loaded!
-		for (var set in obj.sets) {
-		    var paths = obj.sets[set];
-		    var dsts = [];
-		    paths.map(function(path) {
-			var dst = modelObjects.filter(function (c) {
-			    return c.path == path;
-			})[0];
-			if (dst)
-			    dsts.push(dst);
-			else if (path != null)
-			    throw new String(obj.name + ' has set '+set+' containing pointer to object not in tree!');
-			else
-			    throw new String(obj.name + ' has set '+set+' containing null pointer!'); // shouldn't be possible!
-		    });
-		    obj[set] = dsts;
-		}
-	    });
-	},
-	processModel: function(model) {
-	},
+		// create object
+		self.core.createNode({
+		    parent: parentNode,
+		    base: self.META[base]
+		});
+		// configure attributes
+		
+		// add path of object to created objects list
+		self.createdObjects.push(objectPath);
+	    }
+	    return newNode;
+	}
     }
 });
