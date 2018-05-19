37e78bb03655b0ba90628de6b83b2c52903f5dbe
William Emfinger <waemfinger@gmail.com>
2018-05-19 09:48:58 -0500

    closes #262 integrated ui-replay - showed that it works for tracking changes through model editor, code editor, and hfsm viz.

--- config/config.webgme.js
+++ config/config.webgme.js
@@ -16,27 +16,42 @@ config.visualization.decoratorPaths.push(__dirname + '/../node_modules/webgme-hf
 
 config.visualization.panelPaths.push(__dirname + '/../node_modules/webgme-codeeditor/src/visualizers/panels');
 config.visualization.panelPaths.push(__dirname + '/../node_modules/webgme-hfsm/src/visualizers/panels');
+config.visualization.panelPaths.push(__dirname + '/../node_modules/webgme-ui-replay/src/visualizers/panels');
 config.visualization.panelPaths.push(__dirname + '/../src/visualizers/panels');
 
 
-
+config.rest.components['UIRecorder'] = {
+  src: __dirname + '/../node_modules/webgme-ui-replay/src/routers/UIRecorder/UIRecorder.js',
+  mount: 'routers/UIRecorder',
+    options: {
+        mongo: {
+            uri: 'mongodb://127.0.0.1:27017/webgme-ui-recording-data',
+            options: {}
+        }
+    }
+};
 
 // Visualizer descriptors
 config.visualization.visualizerDescriptors.push(__dirname + '/../src/visualizers/Visualizers.json');
 // Add requirejs paths
 config.requirejsPaths = {
+  'UIRecorder': 'node_modules/webgme-ui-replay/src/routers/UIRecorder',
+  'UIReplay': 'panels/UIReplay/UIReplayControllers',
   'HFSMViz': 'panels/HFSMViz/HFSMVizPanel',
   'CodeEditor': 'panels/CodeEditor/CodeEditorPanel',
   'UMLStateMachineDecorator': 'node_modules/webgme-hfsm/src/decorators/UMLStateMachineDecorator',
   'SoftwareGenerator': 'node_modules/webgme-hfsm/src/plugins/SoftwareGenerator',
   'panels': './src/visualizers/panels',
   'widgets': './src/visualizers/widgets',
+  'panels/UIReplay': './node_modules/webgme-ui-replay/src/visualizers/panels/UIReplay',
+  'widgets/UIReplay': './node_modules/webgme-ui-replay/src/visualizers/widgets/UIReplay',
   'panels/HFSMViz': './node_modules/webgme-hfsm/src/visualizers/panels/HFSMViz',
   'widgets/HFSMViz': './node_modules/webgme-hfsm/src/visualizers/widgets/HFSMViz',
   'panels/CodeEditor': './node_modules/webgme-codeeditor/src/visualizers/panels/CodeEditor',
   'widgets/CodeEditor': './node_modules/webgme-codeeditor/src/visualizers/widgets/CodeEditor',
   'webgme-hfsm': './node_modules/webgme-hfsm/src/common',
   'webgme-codeeditor': './node_modules/webgme-codeeditor/src/common',
+  'webgme-ui-replay': './node_modules/webgme-ui-replay/src/common',
   'webgme-rosmod': './src/common'
 };
 
